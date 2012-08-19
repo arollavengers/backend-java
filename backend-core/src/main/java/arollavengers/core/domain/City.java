69b37de4c78566e573fb72c300601c85dc540ba1
Arnauld Loyer <arnauld.loyer@gmail.com>
2012-08-20 00:53:43 +0200

    First ValueObject // enum: City, disease, GraphCity in progress

---
+++ backend-core/src/main/java/arollavengers/core/domain/City.java
@@ -0,0 +1,73 @@
+package arollavengers.core.domain;
+
+import static arollavengers.core.domain.Disease.Black;
+import static arollavengers.core.domain.Disease.Blue;
+import static arollavengers.core.domain.Disease.Orange;
+import static arollavengers.core.domain.Disease.Yellow;
+
+/**
+ * @author <a href="http://twitter.com/aloyer">@aloyer</a>
+ */
+public enum City {
+    Algiers(Black),
+    Baghdad(Black),
+    Cairo(Black),
+    Chennai(Black),
+    Delhi(Black),
+    Istanbul(Black),
+    Karachi(Black),
+    Kolkata(Black),
+    Moscow(Black),
+    Mumbai(Black),
+    Riyadh(Black),
+    Tehran(Black),
+    // ~~~
+    Atlanta(Blue),
+    Chicago(Blue),
+    Essen(Blue),
+    London(Blue),
+    Madrid(Blue),
+    Milan(Blue),
+    NewYork(Blue),
+    Paris(Blue),
+    SanFrancisco(Blue),
+    SaintPetersburg(Blue),
+    Toronto(Blue),
+    Washington(Blue),
+    // ~~~
+    Bangkok(Orange),
+    Beijing(Orange),
+    HoChiMinhCity(Orange),
+    HongKong(Orange),
+    Jakarta(Orange),
+    Manila(Orange),
+    Osaka(Orange),
+    Seoul(Orange),
+    Shanghai(Orange),
+    Sydney(Orange),
+    Taipei(Orange),
+    Tokyo(Orange),
+    // ~~~
+    Bogota(Yellow),
+    BuenosAires(Yellow),
+    Johannesburg(Yellow),
+    Khartoum(Yellow),
+    Kinshasa(Yellow),
+    Lagos(Yellow),
+    Lima(Yellow),
+    LosAngeles(Yellow),
+    MexicoCity(Yellow),
+    Miami(Yellow),
+    Santiago(Yellow),
+    SaoPaulo(Yellow)
+    ;
+
+    private final Disease disease;
+    City(Disease disease) {
+        this.disease = disease;
+    }
+
+    public Disease defaultDisease() {
+        return disease;
+    }
+}
---
+++ backend-core/src/main/java/arollavengers/core/domain/Disease.java
@@ -0,0 +1,11 @@
+package arollavengers.core.domain;
+
+/**
+ * @author <a href="http://twitter.com/aloyer">@aloyer</a>
+ */
+public enum Disease {
+    Black,
+    Yellow,
+    Orange,
+    Blue;
+}
