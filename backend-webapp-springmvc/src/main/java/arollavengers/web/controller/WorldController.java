5a52cc53e25953d814b8c2caecbcf4bb6900e9e0
Arnauld Loyer <arnauld.loyer@gmail.com>
2012-09-16 22:01:46 +0200

    merge ~ tabulation 4 spaces changed...

--- backend-webapp-springmvc/src/main/java/arollavengers/web/controller/WorldController.java
+++ backend-webapp-springmvc/src/main/java/arollavengers/web/controller/WorldController.java
@@ -3,6 +3,7 @@ package arollavengers.web.controller;
 import arollavengers.core.domain.pandemic.Difficulty;
 import arollavengers.core.infrastructure.Id;
 import arollavengers.core.service.pandemic.WorldService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.Assert;
@@ -15,26 +16,14 @@ public class WorldController {
     @Autowired
     WorldService worldService;
 
-<<<<<<< HEAD
-  @RequestMapping("/create")
-  public Id create(Id ownerId, Difficulty difficulty) {
-=======
     @RequestMapping("/create")
     public Id create(Id ownerId, Difficulty difficulty) {
->>>>>>> 9fa960759af9ba826f0bab5eb102e1bf87268e38
 
         Assert.notNull(ownerId, "Owner is mandatory");
         Assert.notNull(difficulty, "difficulty is mandatory");
 
-<<<<<<< HEAD
-    Id worldId = Id.next();
-    worldService.createWorld(worldId, ownerId, difficulty);
-    return worldId;
-  }
-=======
-        //TODO make this controller better
-        // return worldService.createNew(User.withId(ownerId), difficulty);
-        return null;
+        Id worldId = Id.next();
+        worldService.createWorld(worldId, ownerId, difficulty);
+        return worldId;
     }
->>>>>>> 9fa960759af9ba826f0bab5eb102e1bf87268e38
 }
