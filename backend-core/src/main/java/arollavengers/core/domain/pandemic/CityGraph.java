3975e92d3dd810cb986ef8b467889d06b97d5350
Arnauld Loyer <arnauld.loyer@gmail.com>
2012-09-22 13:31:43 +0200

    Complete links definition: not finished yet

--- backend-core/src/main/java/arollavengers/core/domain/pandemic/CityGraph.java
+++ backend-core/src/main/java/arollavengers/core/domain/pandemic/CityGraph.java
@@ -1,5 +1,8 @@
 package arollavengers.core.domain.pandemic;
 
+import static arollavengers.core.domain.pandemic.CityId.*;
+
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.EnumSet;
@@ -10,135 +13,204 @@ import java.util.List;
  */
 public class CityGraph {
 
-  private List<Link> links = new ArrayList<Link>();
-  private List<Link> unmodifiableRoutesView = Collections.unmodifiableList(links);
+    private List<Link> links = new ArrayList<Link>();
+    private List<Link> unmodifiableRoutesView = Collections.unmodifiableList(links);
 
-  /**
-   * @return an unmodifiable view of all the links currently defined.
-   */
-  public List<Link> getLinks() {
-    return unmodifiableRoutesView;
-  }
-
-  /**
-   * Returns the list of the cities that are linked to the specified one.
-   * @param cityId the city from which one wants to retrieve all the linked ones.
-   * @return the list of the cities that are linked
-   */
-  public EnumSet<CityId> listCitiesLinkedTo(CityId cityId) {
-    EnumSet<CityId> cityIds = EnumSet.noneOf(CityId.class);
-    for (Link link : links) {
-      if (link.contains(cityId)) {
-        cityIds.add(link.other(cityId));
-      }
-    }
-    return cityIds;
-  }
-
-  protected void initialize() {
-    // ~~~ Blue
-    define(CityId.SanFrancisco, CityId.Tokyo);
-    define(CityId.SanFrancisco, CityId.Manila);
-    define(CityId.SanFrancisco, CityId.LosAngeles);
-    define(CityId.SanFrancisco, CityId.Chicago);
-    define(CityId.Chicago, CityId.SanFrancisco);
-    define(CityId.Chicago, CityId.LosAngeles);
-    define(CityId.Chicago, CityId.MexicoCity);
-    define(CityId.Chicago, CityId.Atlanta);
-    define(CityId.Chicago, CityId.Toronto);
-    define(CityId.Atlanta, CityId.Chicago);
-    define(CityId.Atlanta, CityId.Miami);
-    define(CityId.Atlanta, CityId.Washington);
-    define(CityId.Toronto, CityId.Chicago);
-    define(CityId.Toronto, CityId.Washington);
-    define(CityId.Toronto, CityId.NewYork);
-    define(CityId.Washington, CityId.NewYork);
-    define(CityId.Washington, CityId.Toronto);
-    define(CityId.Washington, CityId.Atlanta);
-    define(CityId.Washington, CityId.Miami);
-    define(CityId.NewYork, CityId.Toronto);
-    define(CityId.NewYork, CityId.Washington);
-    define(CityId.NewYork, CityId.Madrid);
-    define(CityId.NewYork, CityId.London);
-    define(CityId.Madrid, CityId.NewYork);
-    define(CityId.Madrid, CityId.SaoPaulo);
-    define(CityId.Madrid, CityId.Algiers);
-    define(CityId.Madrid, CityId.Paris);
-    define(CityId.Madrid, CityId.London);
-    define(CityId.London, CityId.NewYork);
-    define(CityId.London, CityId.Madrid);
-    define(CityId.London, CityId.Paris);
-    define(CityId.London, CityId.Essen);
-    define(CityId.Paris, CityId.London);
-    define(CityId.Paris, CityId.Madrid);
-    define(CityId.Paris, CityId.Algiers);
-    define(CityId.Paris, CityId.Milan);
-    define(CityId.Paris, CityId.Essen);
-    define(CityId.Essen, CityId.London);
-    define(CityId.Essen, CityId.Paris);
-    define(CityId.Essen, CityId.Milan);
-    define(CityId.Essen, CityId.SaintPetersburg);
-    define(CityId.Milan, CityId.Essen);
-    define(CityId.Milan, CityId.Paris);
-    define(CityId.Milan, CityId.Istanbul);
-    define(CityId.SaintPetersburg, CityId.Essen);
-    define(CityId.SaintPetersburg, CityId.Istanbul);
-    define(CityId.SaintPetersburg, CityId.Moscow);
-    // ~~~ Black
-    // ~~~ Yellow
-    // ~~~ Orange
-  }
-
-
-  private void define(CityId cityIdOne, CityId cityIdTwo) {
-    Link r = new Link(cityIdOne, cityIdTwo);
-    for (Link link : links) {
-      if (link.sameAs(r)) {
-        return;
-      }
-    }
-    links.add(r);
-  }
-
-  /**
-   * Describe a connection between two cities.
-   */
-  public static class Link {
-    private final CityId cityIdOne;
-    private final CityId cityIdTwo;
-
-    public Link(CityId cityIdOne, CityId cityIdTwo) {
-      this.cityIdOne = cityIdOne;
-      this.cityIdTwo = cityIdTwo;
+    public CityGraph() {
+        initializeBlackLinks();
+        initializeBlueLinks();
+        initializeOrangeLinks();
+        initializeYellowLinks();
     }
 
     /**
-     * Indicates whether or not this route is connected to the specified cityId.
-     *
-     * @param cityId the specified cityId
-     * @return <code>true</code> if this route is connected to the specified cityId.
+     * @return an unmodifiable view of all the links currently defined.
      */
-    public boolean contains(CityId cityId) {
-      return cityIdOne == cityId || cityIdTwo == cityId;
+    public List<Link> getLinks() {
+        return unmodifiableRoutesView;
     }
 
-    public boolean sameAs(Link link) {
-      return (link.cityIdOne == cityIdOne && link.cityIdTwo == cityIdTwo)
-              || (link.cityIdTwo == cityIdOne && link.cityIdOne == cityIdTwo);
+    /**
+     * Returns the list of the cities that are linked to the specified one.
+     *
+     * @param cityId the city from which one wants to retrieve all the linked ones.
+     * @return the list of the cities that are linked
+     */
+    public EnumSet<CityId> listCitiesLinkedTo(CityId cityId) {
+        EnumSet<CityId> cityIds = EnumSet.noneOf(CityId.class);
+        for (Link link : links) {
+            if (link.contains(cityId)) {
+                cityIds.add(link.other(cityId));
+            }
+        }
+        return cityIds;
     }
 
-    @Override
-    public String toString() {
-      return "Link{" + cityIdOne + " <--> " + cityIdTwo + '}';
+    protected void initializeBlueLinks() {
+        // ~~~ Blue
+        define(SanFrancisco, Tokyo);
+        define(SanFrancisco, Manila);
+        define(SanFrancisco, LosAngeles);
+        define(SanFrancisco, Chicago);
+        define(Chicago, SanFrancisco);
+        define(Chicago, LosAngeles);
+        define(Chicago, MexicoCity);
+        define(Chicago, Atlanta);
+        define(Chicago, Toronto);
+        define(Atlanta, Chicago);
+        define(Atlanta, Miami);
+        define(Atlanta, Washington);
+        define(Toronto, Chicago);
+        define(Toronto, Washington);
+        define(Toronto, NewYork);
+        define(Washington, NewYork);
+        define(Washington, Toronto);
+        define(Washington, Atlanta);
+        define(Washington, Miami);
+        define(NewYork, Toronto);
+        define(NewYork, Washington);
+        define(NewYork, Madrid);
+        define(NewYork, London);
+        define(Madrid, NewYork);
+        define(Madrid, SaoPaulo);
+        define(Madrid, Algiers);
+        define(Madrid, Paris);
+        define(Madrid, London);
+        define(London, NewYork);
+        define(London, Madrid);
+        define(London, Paris);
+        define(London, Essen);
+        define(Paris, London);
+        define(Paris, Madrid);
+        define(Paris, Algiers);
+        define(Paris, Milan);
+        define(Paris, Essen);
+        define(Essen, London);
+        define(Essen, Paris);
+        define(Essen, Milan);
+        define(Essen, SaintPetersburg);
+        define(Milan, Essen);
+        define(Milan, Paris);
+        define(Milan, Istanbul);
+        define(SaintPetersburg, Essen);
+        define(SaintPetersburg, Istanbul);
+        define(SaintPetersburg, Moscow);
     }
 
-    public CityId other(CityId cityId) {
-      if (cityId == cityIdOne) {
-        return cityIdTwo;
-      }
-      else {
-        return cityIdOne;
-      }
+    protected void initializeBlackLinks() {
+        // ~~~ Black
+        define(Algiers, Madrid);
+        define(Algiers, Paris);
+        define(Algiers, Istanbul);
+        define(Algiers, Cairo);
+        define(Istanbul, Algiers);
+        define(Istanbul, Milan);
+        define(Istanbul, SaintPetersburg);
+        define(Istanbul, Moscow);
+        define(Istanbul, Baghdad);
+        define(Istanbul, Cairo);
+        define(Moscow, SaintPetersburg);
+        define(Moscow, Tehran);
+        define(Moscow, Istanbul);
+        define(Tehran, Moscow);
+        define(Tehran, Baghdad);
+        define(Tehran, Karachi);
+        define(Tehran, Delhi);
+        define(Delhi, Tehran);
+        define(Delhi, Karachi);
+        define(Delhi, Mumbai);
+        define(Delhi, Chennai);
+        define(Delhi, Kolkata);
+        define(Kolkata, Delhi);
+        define(Kolkata, HongKong);
+        define(Kolkata, Bangkok);
+        define(Kolkata, Chennai);
+        define(Chennai, Kolkata);
+        define(Chennai, Bangkok);
+        define(Chennai, Jakarta);
+        define(Chennai, Mumbai);
+        define(Chennai, Delhi);
+        define(Mumbai, Karachi);
+        define(Mumbai, Delhi);
+        define(Mumbai, Chennai);
+        define(Karachi, Baghdad);
+        define(Karachi, Tehran);
+        define(Karachi, Delhi);
+        define(Karachi, Mumbai);
+        define(Karachi, Riyadh);
+        define(Baghdad, Istanbul);
+        define(Baghdad, Tehran);
+        define(Baghdad, Karachi);
+        define(Baghdad, Riyadh);
+        define(Baghdad, Cairo);
+        define(Cairo, Algiers);
+        define(Cairo, Istanbul);
+        define(Cairo, Baghdad);
+        define(Cairo, Riyadh);
+        define(Cairo, Khartoum);
+        define(Riyadh, Cairo);
+        define(Riyadh, Baghdad);
+        define(Riyadh, Karachi);
     }
-  }
+
+    protected void initializeYellowLinks() {
+        // ~~~ Yellow
+    }
+
+    protected void initializeOrangeLinks() {
+        // ~~~ Orange
+    }
+
+
+    private void define(CityId cityIdOne, CityId cityIdTwo) {
+        Link r = new Link(cityIdOne, cityIdTwo);
+        for (Link link : links) {
+            if (link.sameAs(r)) {
+                return;
+            }
+        }
+        links.add(r);
+    }
+
+    /**
+     * Describe a connection between two cities.
+     */
+    public static class Link {
+        private final CityId cityIdOne;
+        private final CityId cityIdTwo;
+
+        public Link(CityId cityIdOne, CityId cityIdTwo) {
+            this.cityIdOne = cityIdOne;
+            this.cityIdTwo = cityIdTwo;
+        }
+
+        /**
+         * Indicates whether or not this route is connected to the specified cityId.
+         *
+         * @param cityId the specified cityId
+         * @return <code>true</code> if this route is connected to the specified cityId.
+         */
+        public boolean contains(CityId cityId) {
+            return cityIdOne == cityId || cityIdTwo == cityId;
+        }
+
+        public boolean sameAs(Link link) {
+            return (link.cityIdOne == cityIdOne && link.cityIdTwo == cityIdTwo)
+                    || (link.cityIdTwo == cityIdOne && link.cityIdOne == cityIdTwo);
+        }
+
+        @Override
+        public String toString() {
+            return "Link{" + cityIdOne + " <--> " + cityIdTwo + '}';
+        }
+
+        public CityId other(CityId cityId) {
+            if (cityId == cityIdOne) {
+                return cityIdTwo;
+            }
+            else {
+                return cityIdOne;
+            }
+        }
+    }
 }
